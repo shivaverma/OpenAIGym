f0c6ebff67ffb01814bc62044465bba07d911092
shiva verma <shivajbd@gmail.com>
2020-04-30 22:44:25 +0530

    wrong comment changed

--- lunar-lander/discrete/lunar_lander.py
+++ lunar-lander/discrete/lunar_lander.py
@@ -1,7 +1,13 @@
-#  A car is on a one-dimensional track, positioned between two "mountains".
-#  The goal is to drive up the mountain on the right; however,
-#  the car's engine is not strong enough to scale the mountain in a single pass. Therefore,
-#  the only way to succeed is to drive back and forth to build up momentum.
+# Landing pad is always at coordinates (0,0). Coordinates are the first
+# two numbers in state vector. Reward for moving from the top of the screen
+# to landing pad and zero speed is about 100..140 points. If lander moves
+# away from landing pad it loses reward back. Episode finishes if the lander
+# crashes or comes to rest, receiving additional -100 or +100 points.
+# Each leg ground contact is +10. Firing main engine is -0.3 points each frame.
+# Solved is 200 points. Landing outside landing pad is possible. Fuel is
+# infinite, so an agent can learn to fly and then land on its first attempt.
+# Four discrete actions available: do nothing, fire left orientation engine,
+# fire main engine, fire right orientation engine.
 
 
 import gym
